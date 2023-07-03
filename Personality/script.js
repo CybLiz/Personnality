4be1cc936a1650cb7f447a9693f0f0d9c5521177
CybLiz <liza.bahloul@gmail.com>
2023-07-03 11:10:17 +0200

    newupdates

--- Personality/script.js
+++ Personality/script.js
@@ -1,1 +1,39 @@
 // Votre code Javascript
+let button1= document.getElementById("DwayneJohnsonButton")
+let button2= document.getElementById("VinDieselButton")
+let button3= document.getElementById("ImenEsButton")
+let button4= document.getElementById("JessicaAlbaButton")
+let button5= document.getElementById("zitaHanrotButton")
+
+let bw=document.getElementById('bw')
+let color=document.getElementById('color')
+
+
+
+button1.addEventListener('click',function() {
+		document.getElementById("color")
+		
+	})
+
+bw.addEventListener('click',function() {
+		
+		
+	})
+
+
+button2.addEventListener('click', () => {
+	alert("je suis trop forte")
+
+})
+button3.addEventListener('click', () => {
+	alert("je suis trop forte")
+
+})
+button4.addEventListener('click', () => {
+	alert("je suis trop forte")
+
+})
+button5.addEventListener('click', () => {
+	alert("je suis trop forte")
+
+})
